49e4de2118f8c3b28cb1a9cc5f8d901e9afaa202
rezihere <muhammadfahreza78@gmail.com>
2026-08-21 13:51:10 +0700

    Configure passthrough copy and news collection
    Added passthrough copy for static files and a news collection.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,31 @@
 module.exports = function (eleventyConfig) {
 
+    // File statis
     eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("script.js");
     eleventyConfig.addPassthroughCopy("images");
 
+    // Koleksi berita
+    eleventyConfig.addCollection("berita", function (collectionApi) {
+
+        return collectionApi
+            .getFilteredByGlob("content/berita/*.md")
+            .sort(function (a, b) {
+
+                return new Date(b.data.date) - new Date(a.data.date);
+
+            });
+
+    });
+
     return {
+
         dir: {
             input: ".",
             includes: "_includes",
             output: "_site"
         }
+
     };
 
 };
