e003a9f28ff2d93affd79260efc5addea00efd56
rezihere <muhammadfahreza78@gmail.com>
2026-08-21 13:57:41 +0700

    Configure Eleventy with passthrough copies and collection
    Added passthrough copy for style, script, and images. Created a collection for 'berita' sorted by date.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,33 @@
+// Eleventy CMS build
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("style.css");
+    eleventyConfig.addPassthroughCopy("script.js");
+    eleventyConfig.addPassthroughCopy("images");
+
+    eleventyConfig.addCollection("berita", function (collectionApi) {
+
+        return collectionApi
+            .getFilteredByGlob("content/berita/*.md")
+            .sort(function (a, b) {
+
+                return new Date(b.data.date) - new Date(a.data.date);
+
+            });
+
+    });
+
+    return {
+
+        dir: {
+            input: ".",
+            includes: "_includes",
+            output: "_site"
+        }
+
+    };
+
+};
 module.exports = function (eleventyConfig) {
 
     // File statis
